d7e446604924e3176db2e287af4dc27481f802a9
ilkoTheTiger <ilkothetiger@gmail.com>
2023-09-16 23:49:28 +0300

    Server.js refactored to ES6 Modules

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+    logger: true
+})
 
 // Declare a route
-fastify.get('/', function handler(request, reply) {
-    reply.send({ hello: 'world' })
+fastify.get('/', async function handler(request, reply) {
+    return { hello: 'world' }
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, (err) => {
-    if (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
-})
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
