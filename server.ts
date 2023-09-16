0962351f5befb450f1149e9f7efc014d01048856
ilkoTheTiger <ilkothetiger@gmail.com>
2023-09-16 23:51:08 +0300

    Logger added to TypeScript Demo

--- server.ts
+++ server.ts
@@ -1,38 +1,39 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-import { Server, IncomingMessage, ServerResponse } from 'http'
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
+import { Server, IncomingMessage, ServerResponse } from "http";
 
-const server: FastifyInstance = Fastify({})
+const server: FastifyInstance = Fastify({
+    logger: true
+});
 
 const opts: RouteShorthandOptions = {
   schema: {
     response: {
       200: {
-        type: 'object',
+        type: "object",
         properties: {
           pong: {
-            type: 'string'
-          }
-        }
-      }
-    }
-  }
-}
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
 
-server.get('/ping', opts, async (request, reply) => {
-  return { pong: 'it worked!' }
-})
+server.get("/ping", opts, async (request, reply) => {
+  return { pong: "it worked!" };
+});
 
 const start = async () => {
   try {
-    await server.listen({ port: 3000 })
+    await server.listen({ port: 3000 });
 
-    const address = server.server.address()
-    const port = typeof address === 'string' ? address : address?.port
-
+    const address = server.server.address();
+    const port = typeof address === "string" ? address : address?.port;
   } catch (err) {
-    server.log.error(err)
-    process.exit(1)
+    server.log.error(err);
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
